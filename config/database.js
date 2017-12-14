3f14ce031551b554fbe6cf52e0e511792d07ff41
unknown <ahsan.tatiq34@gmail.com>
2017-12-14 15:07:16 +0500

    mlab link

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports= {
-	database: 'mongodb://localhost:27017/meanauth',
+	database: '	database: 'mongodb://repos:ahsan343@ds129344.mlab.com:29344/meanauthapp',',
 	secret: 'yoursecret'
 }
